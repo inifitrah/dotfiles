a151d6aea2bd6736e92ea25f16a281840fff8981
fitrah <mftrhr@gmail.com>
2024-12-05 06:53:10 +0800

    cange colorschema

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -50,6 +50,3 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.g.mapleader = ' '
-
--- Tema (opsional)
-vim.cmd('colorscheme torte')
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -2,10 +2,10 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-       require("lualine").setup({
-    options = {
-        theme = "molokai",
-      }
-       })
+        require("lualine").setup({
+            options = {
+                theme = "nord",
+            }
+        })
     end
 }
