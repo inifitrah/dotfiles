0e1d7030b260686d869c7020951074b0e7239263
fitrah <mftrhr@gmail.com>
2025-02-22 08:18:23 +0800

    Add Calendar widget and enhance Bar component with new features

--- ags/.config/ags/app.ts
+++ ags/.config/ags/app.ts
@@ -1,10 +1,12 @@
-import { App } from "astal/gtk3"
-import style from "./style.scss"
-import Bar from "./widget/Bar"
+import { App } from "astal/gtk3";
+import style from "./style.scss";
+import Bar from "./widget/Bar";
+import Calendar from "./widget/Calendar";
 
 App.start({
-    css: style,
-    main() {
-        App.get_monitors().map(Bar)
-    },
-})
+  css: style,
+  main() {
+    App.get_monitors().map(Bar);
+    Calendar();
+  },
+});
