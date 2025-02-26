c0411200abd62d1ce500d81bbd794786355097ea
fitrah <mftrhr@gmail.com>
2025-02-27 07:21:14 +0800

    Enhance styling and configuration: add green color variable, update Bar component, and enable Polkit authentication daemon

--- ags/.config/ags/widget/Bar.tsx
+++ ags/.config/ags/widget/Bar.tsx
@@ -175,9 +175,7 @@ export default function Bar(monitor: Gdk.Monitor) {
           <Workspaces />
           <FocusedClient />
         </box>
-        <box>
-          <Media />
-        </box>
+        <box>{/* <Media /> */}</box>
         <box hexpand halign={Gtk.Align.END}>
           <SysTray />
           <Wifi />
