af2167f6cbd1a3fbbc12a43589ed44d7fff5c9d4
fitrah <mftrhr@gmail.com>
2025-04-02 17:45:53 +0800

    refactor: Move PowerMenu component to a new file and update import paths for consistency

--- ags/.config/ags/windows.ts
+++ ags/.config/ags/windows.ts
@@ -1,8 +1,8 @@
-import Bar from "./src/components/bar/Bar";
+import Bar from "./src/components/Bar";
 import Calendar from "./src/components/Calendar";
 import Notch from "./src/components/Notch";
 import AudioSlider from "./src/components/AudioSlider";
-import PowerMenu from "./src/components/powerMenu/PowerMenu";
+import PowerMenu from "./src/components/PowerMenu";
 import OSD from "./src/components/OSD";
 
 export default [Bar, Calendar, Notch, AudioSlider, PowerMenu, OSD];
