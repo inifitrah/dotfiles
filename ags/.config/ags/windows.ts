4640a096ba11b17aa4e50e27921576281a2c13a2
fitrah <mftrhr@gmail.com>
2025-04-25 17:57:47 +0800

    refactor: Add guidelines component and update styles for phone guide visibility

--- ags/.config/ags/windows.ts
+++ ags/.config/ags/windows.ts
@@ -4,5 +4,6 @@ import Notch from "./src/components/Notch";
 import AudioSlider from "./src/components/AudioSlider";
 import PowerMenu from "./src/components/PowerMenu";
 import OSD from "./src/components/OSD";
+import GuideLines from "./src/components/GuideLines";
 
-export default [Bar, Calendar, Notch, AudioSlider, PowerMenu, OSD];
+export default [Bar, Calendar, Notch, AudioSlider, PowerMenu, OSD, GuideLines];
