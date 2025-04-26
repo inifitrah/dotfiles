c1e8b8b62005da4e6175169d41882934a8309f96
fitrah <mftrhr@gmail.com>
2025-04-26 20:25:38 +0800

    refactor: Enhance Dynamic Island component with hover effects and improved styling

--- ags/.config/ags/src/components/DynamicIsland.tsx
+++ ags/.config/ags/src/components/DynamicIsland.tsx
@@ -3,24 +3,42 @@ import { Astal, Gtk, Gdk, App } from "astal/gtk3";
 
 // DynamicIsland component inspired by iOS
 const DynamicIsland = (monitor: Gdk.Monitor) => {
+  const isHovered = Variable(false);
+  isHovered.subscribe((value) => {
+    console.log("isHovered", value);
+  });
   return (
     <window
       name={`dynamic-island-${monitor}`}
       className="dynamic-island-window"
       application={App}
       gdkmonitor={monitor}
-      exclusivity={Astal.Exclusivity.EXCLUSIVE}
+      exclusivity={Astal.Exclusivity.NORMAL}
       anchor={Astal.WindowAnchor.TOP}
-      layer={Astal.Layer.OVERLAY}
+      layer={Astal.Layer.TOP}
     >
       <box className="dynamic-island" hpack={Gtk.Align.CENTER}>
         {/* Default state - pill shape */}
-        <box className="dynamic-island-inner">
-          <box className="dynamic-island-content">
-            {/* Placeholder content - can be replaced with actual widgets */}
-            <box className="dynamic-island-indicator" />
+        <eventbox
+          onHover={() => {
+            isHovered.set(true);
+          }}
+          onHoverLost={() => {
+            isHovered.set(false);
+          }}
+        >
+          <box className="dynamic-island-inner">
+            <box className="dynamic-island-content">
+              {/* Placeholder content - can be replaced with actual widgets */}
+              <box className="dynamic-island-indicator" />
+              <box css={"margin-left: 10px"}>
+                <label cursor={"pointer"} className="dynamic-island-label">
+                  Dynamic Island
+                </label>
+              </box>
+            </box>
           </box>
-        </box>
+        </eventbox>
       </box>
     </window>
   );
