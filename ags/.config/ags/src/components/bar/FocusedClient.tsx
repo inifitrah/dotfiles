8b5783b07c607374c468b3ee4c56481109634cd3
fitrah <mftrhr@gmail.com>
2025-05-10 19:43:08 +0800

    feat: Simplify FocusedClient component by improving label rendering and logging focused client changes

--- ags/.config/ags/src/components/bar/FocusedClient.tsx
+++ ags/.config/ags/src/components/bar/FocusedClient.tsx
@@ -5,20 +5,16 @@ export function FocusedClient() {
   const hypr = Hyprland.get_default();
   const focused = bind(hypr, "focusedClient");
 
+  focused.subscribe((client) => {
+    console.log("Focused client changed:", client.title);
+  });
+
   return (
     <box className="Focused" visible={focused.as(Boolean)}>
-      {focused.as(
-        (client) =>
-          client && (
-            <label
-              label={bind(client, "title").as((title) => {
-                const maxLength = 30;
-                return title.length <= maxLength
-                  ? title
-                  : title.substring(0, 30) + "...";
-              })}
-            />
-          )
+      {focused.as((client) =>
+        client
+          ? [<label tooltipMarkup={bind(client, "title")} label={"Client"} />]
+          : undefined
       )}
     </box>
   );
