259c42253aa9cdd62a8900769c96ed2f7936a0c1
fitrah <mftrhr@gmail.com>
2025-05-11 17:26:09 +0800

    feat: Integrate ControlCenter component into app initialization

--- ags/.config/ags/app.ts
+++ ags/.config/ags/app.ts
@@ -1,11 +1,13 @@
 import { App } from "astal/gtk3";
 import style from "./style/style.scss";
 import windows from "./windows";
+import { ControllCenter } from "./src/components/bar/control-center/ControlCenter";
 
 App.start({
   // instanceName: "trah-shell",
   css: style,
   main() {
     windows.map((win) => App.get_monitors().map((mon) => win(mon)));
+    ControllCenter();
   },
 });
