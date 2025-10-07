c8c5e42b403a18e52a0d19dd45327c58b28a445f
fitrah <mftrhr@gmail.com>
2025-10-07 15:41:17 +0800

    feat: Update colorschemes to use github-theme; add lazygit and lsp configurations

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,10 +1,13 @@
 return {
-  "bluz71/vim-moonfly-colors",
-  name = "moonfly",
-  lazy = false,
-  priority = 1000,
+  "projekt0n/github-nvim-theme",
+  name = "github-theme",
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
-    vim.g.moonflyTransparent = true
-    vim.cmd.colorscheme("moonfly")
+    require("github-theme").setup({
+      -- ...
+    })
+
+    vim.cmd("colorscheme github_dark_default")
   end,
 }
