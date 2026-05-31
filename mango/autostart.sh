2533569a1a8f07961576b02ba013d41c3b549b23
fitrah <mftrhr@gmail.com>
2026-05-31 21:56:21 +0800

    update mango config

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -2,6 +2,11 @@
 
 set +e
 /usr/lib/xdg-desktop-portal-wlr  >/dev/null 2>&1 &
-awww-daemon >/dev/null 2>&1 &
-vibepanel >/dev/null 2>&1 &
+echo "Xft.dpi: 100" | xrdb -merge
+gsettings set org.gnome.desktop.interface text-scaling-factor 1
+noctalia >/dev/null 2>&1 &
+# awww-daemon >/dev/null 2>&1 &
+# qs -c noctalia-shell >/dev/null 2>&1 &
+# awww-daemon >/dev/null 2>&1 &
+# vibepanel >/dev/null 2>&1 &
 vicinae server >/dev/null 2>&1 &
