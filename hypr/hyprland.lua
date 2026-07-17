b8dfae940c86b54a6f343529a3c1652c57832dae
fitrah <mftrhr@gmail.com>
2026-07-17 23:30:37 +0800

    config(hyprland): Add the hyprbars plugin and customize it with a macOS style

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -440,5 +440,52 @@ hl.window_rule({
     float = true,
 })
 
+-----------------
+---- PLUGINS  ---
+-----------------
+hl.config({
+    plugin = {
+        hyprbars = {
+                  bar_height                 = 32,
+                  bar_part_of_window         = true,
+                  bar_buttons_alignment      = "left",
+                  bar_button_padding         = 10,
+                  bar_blur                   = false,
+                  bar_padding                = 12	,
+                  bar_text_font              = "",
+                  bar_text_size			   = 14,
+                  bar_precedence_over_border = true,
+                  bar_color                  = "rgba(00000000)",
+                  col                        = {
+                      text = "rgb(ffffff)",
+                  },
+                  icon_on_hover              = true,
+                  inactive_button_color      = "rgb(c2c2c2)",
+                  on_double_click            = "hyprctl dispatch 'hl.dsp.window.fullscreen_state({internal = 1, client = 0})'"
+              },
+    }
+})
+hl.plugin.hyprbars.add_button({
+    bg_color = "rgb(fe5154)",
+    fg_color = "rgb(000000)",
+    size = 16,
+    icon = "󰖭",
+    action = "hyprctl dispatch 'hl.dsp.window.close()'",
+})
+hl.plugin.hyprbars.add_button({
+    bg_color = "rgb(f7c000)",
+    fg_color = "rgb(000000)",
+    size = 16,
+    icon = "",
+    action = "hyprctl dispatch 'hl.dsp.window.float()'",
+})
+hl.plugin.hyprbars.add_button({
+    bg_color = "rgb(2dbf4d)",
+    fg_color = "rgb(000000)",
+    size = 16,
+    icon = "󰘖",
+    action = "hyprctl dispatch 'hl.dsp.window.fullscreen_state({internal = 1, client = 0})'"
+})
+
 -- For Noctalia Color templates
 require("noctalia").apply_theme()
