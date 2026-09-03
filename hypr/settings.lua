7a8f6aa41d9a7de5c823966291366a042655c592
fitrah <mftrhr@gmail.com>
2026-09-03 09:22:37 +0800

    refactor(hypr): consolidate input and misc into settings
    - hapus hypr/input.lua dan hypr/misc.lua
    - pindahkan config input (kb_layout, follow_mouse, touchpad, gesture, device)
      dan misc (force_default_wallpaper, disable_hyprland_logo, focus_on_activate,
      binds.hide_special_on_workspace_change) ke hypr/settings.lua
    - hapus require('misc') dan require('input') dari hypr/hyprland.lua
    - settings sekarang jadi single source untuk look+feel, input, misc

--- hypr/settings.lua
+++ hypr/settings.lua
@@ -1,8 +1,3 @@
------------------------
----- LOOK AND FEEL ----
------------------------
-
--- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
         gaps_in  = 5,
@@ -18,14 +13,12 @@ hl.config({
         -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
         resize_on_border = false,
 
-        -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
         allow_tearing = false,
 
         layout = "scrolling",
 
         no_focus_fallback = true
     },
-
     decoration = {
         rounding       = 20,
         rounding_power = 2,
@@ -49,11 +42,35 @@ hl.config({
             brightness = 0.4
         },
     },
+    input = {
+        kb_layout  = "us",
+        kb_variant = "",
+        kb_model   = "",
+        kb_options = "",
+        kb_rules   = "",
 
+        follow_mouse = 1,
+
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad = {
+            natural_scroll = false,
+        },
+        mouse_refocus = false
+    },
+    misc = {
+        force_default_wallpaper = 0,   -- Set to 0 or 1 to disable the anime mascot wallpapers
+        disable_hyprland_logo   = true, -- If true disables the random hyprland logo / anime girl background. :(
+        focus_on_activate       = true,
+        font_family             = "NotoSans Nerd Font",
+        disable_autoreload      = false
+    },
+    binds = {
+        hide_special_on_workspace_change = true
+    },
     animations = {
         enabled = true,
     },
-
     cursor = {
         no_warps = false
     },
